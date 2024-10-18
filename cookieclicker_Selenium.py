c26fc29bed44735cb4ae78a999f73c017e160213
Iman <imangholamiimi@gmail.com>
2024-10-18 12:32:54 +0330

    :sparkles: | ADD | saving recovery code and ready to play

--- cookieclicker_Selenium.py
+++ cookieclicker_Selenium.py
@@ -10,36 +10,47 @@ from decouple import config
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
+chrome_options.add_argument('--window-size=600,700')
 driver = webdriver.Chrome(options=chrome_options)
 
 driver.get('https://orteil.dashnet.org/cookieclicker/')
 
 # select lng
-eng_btn = WebDriverWait(driver, 10).until(
+eng_btn = WebDriverWait(driver, 60).until(
     EC.element_to_be_clickable((By.ID, 'langSelect-EN'))
 )
 eng_btn.click()
 
 # make a save address. COPY and Save it to use later
+sleep(1)
 option_btn = driver.find_element(By.XPATH, '//*[@id="prefsButton"]/div')
 option_btn.click()
-export_save_code = WebDriverWait(driver, 10).until(
+sleep(1)
+export_save_code = WebDriverWait(driver, 60).until(
     EC.element_to_be_clickable((By.XPATH, '//*[@id="menu"]/div[3]/div/div[4]/a[1]'))
 )
 export_save_code.click()
 
 # Wait for the prompt and extract the save code
-save_code = WebDriverWait(driver, 10).until(
+save_code = WebDriverWait(driver, 60).until(
     EC.presence_of_element_located((By.XPATH, '//*[@id="textareaPrompt"]'))
 ).text
 
 # Copy save code to clipboard using pyperclip
 pyperclip.copy(save_code)
+code = pyperclip.paste()
+print(code)
 # Display a message to the user in the browser
-driver.execute_script('alert("Your Save code has been copied to the clipboard!Save it to use it later.");')
-sleep(5)
-
-done_btn = driver.find_element(By.XPATH, '//*[@id="promptOption0"]')
+sleep(1)
+done_btn = WebDriverWait(driver, timeout=60).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="promptClose"]'))
+)
 done_btn.click()
-
-driver.quit()
+sleep(1)
+driver.execute_script('alert("Your Save code has been copied to the clipboard and recovery_code.txt!");')
+sleep(3)
+Keys.ENTER
+## TODO1 write the code in a file
+## TODO2 find cookie and write codes to click it for 5 minutes or a time
+## TODO3 remove quit to manage it by user
+# driver.quit()
